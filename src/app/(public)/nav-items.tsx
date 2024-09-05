d391d462315f00532457ea3da9f2cbce4789718b
padi-dev-manhtd <manhtd@paditech.com>
2024-09-05 21:11:49 +0700

    update item menu when user or manager

--- src/app/(public)/nav-items.tsx
+++ src/app/(public)/nav-items.tsx
@@ -1,46 +1,80 @@
 "use client";
 
 import { useAppContext } from "@/components/app-provider";
-import { getAccessTokenFromLocalstorage } from "@/lib/utils";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+import { Role } from "@/constants/type";
+import { cn, handleErrorApi } from "@/lib/utils";
+import { useLogoutMutation } from "@/queries/useAuth";
+import { RoleType } from "@/types/jwt.types";
 
-const menuItems = [
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+const menuItems: {
+  title: string;
+  href: string;
+  role?: RoleType[];
+  hideWhenLogin?: boolean;
+}[] = [
   {
-    title: "Món ăn",
-    href: "/menu",
-    authRequired: true,
+    title: "Trang chủ",
+    href: "/",
   },
   {
-    title: "Đơn hàng",
-    href: "/orders",
-    authRequired: true,
+    title: "Menu",
+    href: "/guest/menu",
+    role: [Role.Guest],
   },
   {
     title: "Đăng nhập",
     href: "/login",
-    authRequired: false,
+    hideWhenLogin: true,
   },
   {
     title: "Quản lý",
     href: "/manage/dashboard",
-    authRequired: true,
+    role: [Role.Owner, Role.Employee],
   },
 ];
 
 export default function NavItems({ className }: { className?: string }) {
-  const { isAuth } = useAppContext();
-
-  return menuItems.map((item) => {
-    if (
-      (item.authRequired === false && isAuth) ||
-      (item.authRequired === true && !isAuth)
-    )
-      return null;
-    return (
-      <Link href={item.href} key={item.href} className={className}>
-        {item.title}
-      </Link>
-    );
-  });
+  const { role, setRole } = useAppContext();
+  const router = useRouter();
+  const logoutMutation = useLogoutMutation();
+  const logout = async () => {
+    if (logoutMutation.isPending) return;
+    try {
+      await logoutMutation.mutateAsync();
+      setRole();
+      router.push("/");
+    } catch (error) {
+      handleErrorApi({
+        error: error,
+      });
+    }
+  };
+  return (
+    <>
+      {menuItems.map((item) => {
+        // trường hợp đăng nhập chỉ hiển thị menu đăng nhập
+        const isAuth = item.role && role && item.role.includes(role);
+        //truowngf hợp menu hiển thị dù đã đăng nhập hay chưa
+        const canShow =
+          (item.role === undefined && !item.hideWhenLogin) ||
+          (!role && item.hideWhenLogin);
+        if (isAuth || canShow) {
+          return (
+            <Link href={item.href} key={item.href} className={className}>
+              {item.title}
+            </Link>
+          );
+        }
+        return null;
+      })}
+      {role && (
+        <div className={cn(className, "cursor-pointer")} onClick={logout}>
+          Đăng xuất
+        </div>
+      )}
+    </>
+  );
 }
