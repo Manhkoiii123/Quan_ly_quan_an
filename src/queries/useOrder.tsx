97d1286758ba01873a3c1f4728d3302e63c8190e
padi-dev-manhtd <manhtd@paditech.com>
2024-09-05 21:49:25 +0700

    declaration api order

---
+++ src/queries/useOrder.tsx
@@ -0,0 +1,21 @@
+import guestApiRequest from "@/apiRequest/guest";
+import orderApiRequest from "@/apiRequest/order";
+import { UpdateOrderBodyType } from "@/schemaValidations/order.schema";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+export const useUpdateOrderMutation = () => {
+  return useMutation({
+    mutationFn: ({
+      orderId,
+      ...body
+    }: UpdateOrderBodyType & {
+      orderId: number;
+    }) => orderApiRequest.updateOrder(orderId, body),
+  });
+};
+export const useGetOrderListQuery = () => {
+  return useQuery({
+    queryFn: orderApiRequest.getOrderList,
+    queryKey: ["guest-orders"],
+  });
+};
